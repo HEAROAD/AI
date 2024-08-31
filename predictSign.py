7bd1a8c9294b67d7726e95e1e3c8f4065fdf20b5
Sherry <100350128+Chaelii@users.noreply.github.com>
2024-08-31 21:14:54 +0900

    FIX: 데이터 반환 타입, 경로

--- predictSign.py
+++ predictSign.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse
 import uvicorn
 import numpy as np
 import cv2
@@ -8,13 +8,24 @@ from tensorflow.keras.models import load_model
 import pickle
 import os
 import shutil
+import json
 
 app = FastAPI()
 
+# 현재 파일이 위치한 디렉토리 경로 얻기
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # 모델과 레이블 인코더 로드
-model = load_model('/best_model.keras')
-with open('/label_encoder.pkl', 'rb') as f:
+model_path = os.path.join(BASE_DIR, 'best_model.keras')
+model = load_model(model_path)
+label_encoder_path = os.path.join(BASE_DIR, 'label_encoder.pkl')
+with open(label_encoder_path, 'rb') as f:
     label_encoder = pickle.load(f)
+
+# 레이블 리스트 로드
+labels_list_path = os.path.join(BASE_DIR, 'labels_list.json')
+with open(labels_list_path, 'r', encoding='utf-8') as f:
+    labels_list = json.load(f)
 
 # 비디오에서 키포인트 추출 함수
 def extract_keypoints_from_video(video_path):
@@ -87,13 +98,13 @@ async def predict_sign(file: UploadFile = File(...)):
     # 예측 수행
     predictions = model.predict(keypoints)
     predicted_label = np.argmax(np.mean(predictions, axis=0))  # 각 프레임의 예측값을 평균내어 최종 예측 결정
-    predicted_word = label_encoder.inverse_transform([predicted_label])[0]
+    predicted_word = labels_list[predicted_label]  # labels_list에서 해당 예측값에 매칭되는 텍스트 찾기
 
     # 임시 파일 삭제
     os.remove(video_path)
 
-    # 결과 반환
-    return JSONResponse(content={"predicted_word": predicted_word})
+    # 결과를 텍스트로 반환
+    return PlainTextResponse(predicted_word)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
